86890ab4f865b06dceab39bdf937fda24a7570db
keldibekov3222 <keldibekovilimbek@gmail.com>
2020-05-11 08:39:14 +0300

    keldibekov3222
    dsa

---
+++ wp-content/themes/Raritet/footer.php
@@ -0,0 +1,75 @@
+<footer class="footer">
+			<div class="footer__text">
+				<div class="footer__text-item">
+					<div class="footer__text-item-icon">
+						<svg width="1em" height="1em" class="icon icon-location ">
+							<svg id="icon-location" viewBox="0 0 6.2 8.1">
+								<path fill="currentColor"
+									d="M3.1,0C1.4,0,0,1.4,0,3.1s2.3,5,3.1,5s3.1-3.3,3.1-5C6.3,1.4,4.9,0,3.1,0z M3.1,4.3c-0.7,0-1.2-0.6-1.2-1.2  c0-0.7,0.6-1.2,1.2-1.2c0.7,0,1.2,0.6,1.2,1.2C4.4,3.8,3.8,4.3,3.1,4.3z">
+								</path>
+							</svg>
+						</svg>
+					</div>
+					<span>
+						Москва, ул. Новослободская, д. 20, офис 301 <span class="mobile--hidden">(м. Новослободская / м.
+							Менделеевская)</span>
+					</span>
+				</div>
+				<div class="footer__text-group">
+					<div class="footer__text-group-left">
+						<a href="tel:+74951200840" class="footer__text-item send-goal" data-goal="SALE_CLICK">
+							<div class="footer__text-item-icon">
+								<svg width="1em" height="1em" class="icon icon-phone ">
+									<svg id="icon-phone" viewBox="0 0 512 512" fill="none" stroke="currentColor"
+										stroke-width="15">
+										<path fill="currentColor"
+											d="M511.8,404c1.1,8.3-1.4,15.5-7.6,21.7l-72.1,71.5c-3.3,3.6-7.5,6.7-12.7,9.2c-5.2,2.5-10.4,4.2-15.4,4.9   c-0.4,0-1.4,0.1-3.3,0.3c-1.8,0.2-4.2,0.3-7,0.3c-6.9,0-18-1.2-33.3-3.5c-15.4-2.3-34.1-8.1-56.4-17.3   c-22.2-9.2-47.4-23-75.6-41.5c-28.2-18.4-58.2-43.7-90-75.9c-25.3-24.9-46.3-48.8-62.9-71.5c-16.6-22.8-30-43.8-40.1-63.1   c-10.1-19.3-17.7-36.9-22.8-52.6s-8.5-29.3-10.3-40.7s-2.5-20.3-2.2-26.8c0.4-6.5,0.5-10.1,0.5-10.8C1.4,103,3,97.8,5.5,92.6   s5.6-9.5,9.2-12.7L86.8,7.8c5.1-5.1,10.8-7.6,17.3-7.6c4.7,0,8.9,1.4,12.5,4.1s6.7,6.1,9.2,10l58,110c3.3,5.8,4.2,12.1,2.7,19   c-1.4,6.9-4.5,12.6-9.2,17.3l-26.6,26.6c-0.7,0.7-1.4,1.9-1.9,3.5c-0.5,1.6-0.8,3-0.8,4.1c1.4,7.6,4.7,16.3,9.8,26   c4.3,8.7,11,19.2,20.1,31.7s21.9,26.8,38.5,43.1c16.3,16.6,30.7,29.5,43.4,38.8c12.6,9.2,23.2,16,31.7,20.3s15,7,19.5,7.9l6.8,1.4   c0.7,0,1.9-0.3,3.5-0.8c1.6-0.5,2.8-1.2,3.5-1.9l30.9-31.4c6.5-5.8,14.1-8.7,22.8-8.7c6.1,0,11,1.1,14.6,3.3h0.5l104.6,61.8   C505.8,390.8,510.3,396.7,511.8,404z">
+										</path>
+									</svg>
+								</svg>
+							</div>
+							<span>7 (495) 120-08-40</span>
+						</a>
+						<a href="mailto:gold@raritetus.ru" class="footer__text-item send-goal" data-goal="SALE_CLICK">
+							<div class="footer__text-item-icon">
+								<svg width="1em" height="1em" class="icon icon-mail ">
+									<svg id="icon-mail" viewBox="0 0 242.5 171.9" fill="none" stroke="currentColor"
+										stroke-width="15">
+										<path fill="currentColor"
+											d="M242.5,57.6v102.2c0,6.7-5.4,12.1-12.1,12.1H12.1c-6.7,0-12.1-5.4-12.1-12.1V57.6c0-10.2,11.9-15.8,19.8-9.3   l93.9,78.3c4.5,3.7,11,3.7,15.5,0l93.6-78.3C230.6,41.8,242.5,47.4,242.5,57.6z">
+										</path>
+										<path fill="currentColor" d="M242.5,0L129.2,94.8c-4.5,3.7-11,3.7-15.5,0L0.1,0H242.5z">
+										</path>
+									</svg>
+								</svg>
+							</div>
+							<span>gold@raritetus.ru</span>
+						</a>
+					</div>
+					<div class="footer__text-group-center">
+						<div class="footer__text-media">
+							<a href="#" data-link="}bk~ykzz0%%yodn%5zbedo7=>3?;8::2>:" data-goal="SALE_CLICK"
+								rel="nofollow noreferrer" data-messenger="w" class="media-wa send-goal messenger"
+								target="_blank">
+								<svg width="1em" height="1em" class="icon icon-wa ">
+									<svg id="icon-wa" viewBox="0 0 24 24">
+										<path fill="currentColor"
+											d="M17.5,14.4c-0.3-0.1-1.8-0.9-2-1c-0.3-0.1-0.5-0.1-0.7,0.1c-0.2,0.3-0.8,1-0.9,1.2c-0.2,0.2-0.3,0.2-0.6,0.1  c-0.3-0.1-1.3-0.5-2.4-1.5c-0.9-0.8-1.5-1.8-1.7-2.1c-0.2-0.3,0-0.5,0.1-0.6c0.1-0.1,0.3-0.3,0.5-0.5C9.9,9.9,9.9,9.8,10,9.6  c0.1-0.2,0-0.4,0-0.5C9.9,8.9,9.3,7.5,9.1,6.9C8.8,6.3,8.6,6.4,8.4,6.4c-0.2,0-0.4,0-0.6,0S7.3,6.4,7,6.7C6.8,7,6,7.7,6,9.2  s1.1,2.9,1.2,3.1c0.1,0.2,2.1,3.2,5.1,4.5c0.7,0.3,1.3,0.5,1.7,0.6c0.7,0.2,1.4,0.2,1.9,0.1c0.6-0.1,1.8-0.7,2-1.4  c0.3-0.7,0.3-1.3,0.2-1.4C18,14.5,17.8,14.4,17.5,14.4 M12.1,21.8L12.1,21.8c-1.8,0-3.5-0.5-5.1-1.4l-0.4-0.2l-3.8,1l1-3.6l-0.2-0.4  c-1-1.6-1.5-3.4-1.5-5.3C2.1,6.4,6.6,2,12.1,2c2.7,0,5.1,1,7,2.9c1.9,1.9,2.9,4.3,2.9,7C22,17.3,17.5,21.8,12.1,21.8 M20.5,3.4  C18.2,1.2,15.2,0,12,0C5.5,0,0.1,5.3,0.1,11.9c0,2.1,0.5,4.1,1.6,5.9L0,24l6.3-1.7c1.7,0.9,3.7,1.4,5.7,1.4h0  c6.6,0,11.9-5.3,11.9-11.9C24,8.7,22.8,5.7,20.5,3.4">
+										</path>
+									</svg>
+								</svg>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer__map">
+				<div id="YaMaps" class="magazine-map js-map">
+					<div class="magazine-map__container"></div>
+				</div>
+			</div>
+		</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/Raritet/header.php
@@ -0,0 +1,220 @@
+<?php
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php
+	wp_register_style('fonts', 'http://allfont.ru/allfont.css?fonts=heuristica-regular', false, null);
+	wp_enqueue_style('fonts');
+	?>
+	<script>
+	(function() {
+		function addMainJsInsertedCounters(callback) {
+			callback();
+		}
+		window.addMainJsInsertedCounters = addMainJsInsertedCounters;
+	})();
+
+	function insertAsyncScript(f) {
+		(function(i, s, o, g, a, m) {
+			a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', f);
+	}
+	</script>
+	<script>
+	(function(i, g, r) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		i.addMainJsInsertedCounters(function() {
+			i.insertAsyncScript(g)
+		})
+	})(window, 'https://www.google-analytics.com/analytics.js', 'ga');
+	ga('create', 'UA-48865919-1', {
+		cookieDomain: 'raritetus.ru'
+	});
+	ga('require', 'displayfeatures');
+	ga('send', 'pageview')
+	</script>
+	<script type="text/javascript">
+	window.rym = {
+		"counterId": 24135358
+	};
+	(function(m, r, i) {
+		m[i] = m[i] || function() {
+			(m[i].a = m[i].a || []).push(arguments)
+		};
+		m[i].l = 1 * new Date();
+		m.addMainJsInsertedCounters(function() {
+			m.insertAsyncScript(r)
+		})
+	})(window, "https://mc.yandex.ru/metrika/tag.js", "ym");
+	ym(24135358, "init", {
+		clickmap: false,
+		trackLinks: true,
+		webvisor: false,
+		accurateTrackBounce: true,
+		triggerEvent: true,
+	});
+	</script>
+	<noscript>
+		<div><img src="https://mc.yandex.ru/watch/24135358" style="position:absolute; left:-9999px;" alt="" /></div>
+	</noscript>
+	<script type="text/javascript">
+	(function ct_load_script() {
+		var ct = document.createElement('script');
+		ct.type = 'text/javascript';
+		ct.src = document.location.protocol + '//cc.calltracking.ru/phone.67e14.9066.async.js?nc=' + Math.floor(
+			new Date().getTime() / 300000);
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ct, s);
+	})();
+	</script>
+	<?php
+	wp_register_style('fonts', 'http://allfont.ru/allfont.css?fonts=heuristica-Reular', false, null);
+	wp_enqueue_style('fonts');
+	?>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<header class="header js-header">
+		<div class="container">
+			<div class="header__inner" itemscope itemtype="https://schema.org/Organization">
+				<div class="header__inner-part">
+					<div class="header__logo">
+						<a href="/" target="_blank">
+							<img
+								src="<?php echo get_template_directory_uri() ?>/bundles/raritetuscoins/gold/img/tmp_file/46.png"
+								alt="picture" class="header__logo-desktop" /><img
+								src="bundles/raritetuscoins/gold/img/tmp_file/47.png" alt="picture"
+								class="header__logo-mobile" />
+							<span style="display: none;" itemprop="name">Нумизматический клуб Raritetus</span>
+						</a>
+					</div>
+					<div class="header__location" itemscope itemtype="https://schema.org/PostalAddress" itemprop="address">
+						<span>
+							<span itemprop="addressLocality">Москва</span>,
+							<span itemprop="streetAddress">ул. Новослободская, д. 20, офис 301</span>
+							<span>(м. Новослободская / м. Менделеевская)</span>
+						</span>
+					</div>
+				</div>
+				<div class="header__inner-part">
+					<div class="header__media">
+						<a href="#" data-link="}bk~ykzz0%%yodn%5zbedo7=>3?;8::2>:" data-goal="SALE_CLICK"
+							class="send-goal messenger" target="_blank" rel="nofollow noreferrer" data-messenger="w">
+							<svg width="1em" height="1em" class="icon icon-wa ">
+								<svg id="icon-wa" viewBox="0 0 24 24">
+									<path fill="currentColor"
+										d="M17.5,14.4c-0.3-0.1-1.8-0.9-2-1c-0.3-0.1-0.5-0.1-0.7,0.1c-0.2,0.3-0.8,1-0.9,1.2c-0.2,0.2-0.3,0.2-0.6,0.1  c-0.3-0.1-1.3-0.5-2.4-1.5c-0.9-0.8-1.5-1.8-1.7-2.1c-0.2-0.3,0-0.5,0.1-0.6c0.1-0.1,0.3-0.3,0.5-0.5C9.9,9.9,9.9,9.8,10,9.6  c0.1-0.2,0-0.4,0-0.5C9.9,8.9,9.3,7.5,9.1,6.9C8.8,6.3,8.6,6.4,8.4,6.4c-0.2,0-0.4,0-0.6,0S7.3,6.4,7,6.7C6.8,7,6,7.7,6,9.2  s1.1,2.9,1.2,3.1c0.1,0.2,2.1,3.2,5.1,4.5c0.7,0.3,1.3,0.5,1.7,0.6c0.7,0.2,1.4,0.2,1.9,0.1c0.6-0.1,1.8-0.7,2-1.4  c0.3-0.7,0.3-1.3,0.2-1.4C18,14.5,17.8,14.4,17.5,14.4 M12.1,21.8L12.1,21.8c-1.8,0-3.5-0.5-5.1-1.4l-0.4-0.2l-3.8,1l1-3.6l-0.2-0.4  c-1-1.6-1.5-3.4-1.5-5.3C2.1,6.4,6.6,2,12.1,2c2.7,0,5.1,1,7,2.9c1.9,1.9,2.9,4.3,2.9,7C22,17.3,17.5,21.8,12.1,21.8 M20.5,3.4  C18.2,1.2,15.2,0,12,0C5.5,0,0.1,5.3,0.1,11.9c0,2.1,0.5,4.1,1.6,5.9L0,24l6.3-1.7c1.7,0.9,3.7,1.4,5.7,1.4h0  c6.6,0,11.9-5.3,11.9-11.9C24,8.7,22.8,5.7,20.5,3.4">
+									</path>
+								</svg>
+							</svg>
+						</a>
+					</div>
+					<div class="header__contacts">
+						<a itemprop="telephone" href="tel:+74951200840" data-goal="SALE_CLICK"
+							class="header__contacts-line send-goal" target="_blank">
+							<svg width="1em" height="1em" class="icon icon-phone ">
+								<svg id="icon-phone" viewBox="0 0 512 512" fill="none" stroke="currentColor" stroke-width="15">
+									<path fill="currentColor"
+										d="M511.8,404c1.1,8.3-1.4,15.5-7.6,21.7l-72.1,71.5c-3.3,3.6-7.5,6.7-12.7,9.2c-5.2,2.5-10.4,4.2-15.4,4.9   c-0.4,0-1.4,0.1-3.3,0.3c-1.8,0.2-4.2,0.3-7,0.3c-6.9,0-18-1.2-33.3-3.5c-15.4-2.3-34.1-8.1-56.4-17.3   c-22.2-9.2-47.4-23-75.6-41.5c-28.2-18.4-58.2-43.7-90-75.9c-25.3-24.9-46.3-48.8-62.9-71.5c-16.6-22.8-30-43.8-40.1-63.1   c-10.1-19.3-17.7-36.9-22.8-52.6s-8.5-29.3-10.3-40.7s-2.5-20.3-2.2-26.8c0.4-6.5,0.5-10.1,0.5-10.8C1.4,103,3,97.8,5.5,92.6   s5.6-9.5,9.2-12.7L86.8,7.8c5.1-5.1,10.8-7.6,17.3-7.6c4.7,0,8.9,1.4,12.5,4.1s6.7,6.1,9.2,10l58,110c3.3,5.8,4.2,12.1,2.7,19   c-1.4,6.9-4.5,12.6-9.2,17.3l-26.6,26.6c-0.7,0.7-1.4,1.9-1.9,3.5c-0.5,1.6-0.8,3-0.8,4.1c1.4,7.6,4.7,16.3,9.8,26   c4.3,8.7,11,19.2,20.1,31.7s21.9,26.8,38.5,43.1c16.3,16.6,30.7,29.5,43.4,38.8c12.6,9.2,23.2,16,31.7,20.3s15,7,19.5,7.9l6.8,1.4   c0.7,0,1.9-0.3,3.5-0.8c1.6-0.5,2.8-1.2,3.5-1.9l30.9-31.4c6.5-5.8,14.1-8.7,22.8-8.7c6.1,0,11,1.1,14.6,3.3h0.5l104.6,61.8   C505.8,390.8,510.3,396.7,511.8,404z">
+									</path>
+								</svg>
+							</svg>
+							<span>7 (495) 120-08-40</span>
+						</a>
+						<a itemprop="email" href="mailto:gold@raritetus.ru" data-goal="SALE_CLICK"
+							class="header__contacts-line send-goal">
+							<svg width="1em" height="1em" class="icon icon-mail ">
+								<svg id="icon-mail" viewBox="0 0 242.5 171.9" fill="none" stroke="currentColor"
+									stroke-width="15">
+									<path fill="currentColor"
+										d="M242.5,57.6v102.2c0,6.7-5.4,12.1-12.1,12.1H12.1c-6.7,0-12.1-5.4-12.1-12.1V57.6c0-10.2,11.9-15.8,19.8-9.3   l93.9,78.3c4.5,3.7,11,3.7,15.5,0l93.6-78.3C230.6,41.8,242.5,47.4,242.5,57.6z">
+									</path>
+									<path fill="currentColor" d="M242.5,0L129.2,94.8c-4.5,3.7-11,3.7-15.5,0L0.1,0H242.5z">
+									</path>
+								</svg>
+							</svg>
+							<span>gold@raritetus.ru</span>
+						</a>
+					</div>
+					<div class="header__basket">
+						<button type="button" class="basket_menu-open">
+							<svg width="1em" height="1em" class="icon icon-basket ">
+								<svg id="icon-basket" viewBox="0 0 241.4 207.9" fill="none" stroke="currentColor"
+									stroke-width="15">
+									<polyline fill="none" stroke="currentColor" stroke-width="15"
+										points="239.2,5.5 204.5,5.5 168.5,161.8 28.5,161.8 "></polyline>
+									<polyline fill="none" stroke="currentColor" stroke-width="15"
+										points="177.9,121.1 28.5,121.1 12.8,53 193.5,53 "></polyline>
+									<circle fill="currentColor" cx="146.8" cy="193.7" r="12.2"></circle>
+									<circle fill="currentColor" cx="49.9" cy="193.7" r="12.2"></circle>
+								</svg>
+							</svg><span class="basket-item-count">0</span>
+						</button>
+						<form action="/zolotye-monety/order/" method="post" data-goal="SALE_FORM"
+							class="header__basket-inner js-form-check check-basket">
+							<div class="header__basket-inner-ttl"><span>Оформление заказа</span>
+								<button type="button" class="js-exit-btn basket_menu-close">
+									<svg width="1em" height="1em" class="icon icon-close ">
+										<svg id="icon-close-full" viewBox="0 0 82 82" fill="none" stroke="currentColor"
+											stroke-width="2">
+											<circle fill="currentColor" cx="41" cy="41" r="41"></circle>
+											<polyline fill="none" stroke="#fff" points="55.2,55.2 41,41 26.7,26.8  "></polyline>
+											<polyline fill="none" stroke="#fff" points="26.7,55.2 41,41 55.2,26.8  "></polyline>
+										</svg>
+									</svg>
+									<svg width="1em" height="1em" class="icon icon-close-full ">
+										<use
+											xlink:href="bundles/raritetuscoins/gold/img/useful/svg/theme/symbol-defs.svg#icon-close-full">
+										</use>
+									</svg>
+								</button>
+							</div>
+							<div class="header__basket-inner-content">
+							</div>
+							<div class="header__basket-inner-result">
+								<span>Итого:</span>
+								<span class="js-counter-block-total-sum">0₽</span>
+							</div>
+							<div class="header__basket-inner-inputs js-form-check">
+								<div class="form-group group-empty">
+									<div class="input-wrap">
+										<input type="text" name="user_name" placeholder="Имя" value="" required="required"
+											id="user_name" class="form-control" />
+									</div>
+								</div>
+								<div class="form-group group-empty">
+									<div class="input-wrap">
+										<input type="text" name="user_phone" placeholder="Номер телефона" value=""
+											required="required" id="user_phone" data-mask="+ 7 999 999-99-99"
+											class="form-control js-mask" />
+									</div>
+								</div>
+								<div class="form-contact__form-inner-bottom">
+									<button type="submit" value="Send" class="button"><span>ОФОРМИТЬ ЗАКАЗ</span></button>
+									<div class="lds-default">
+										<div></div>
+									</div>
+								</div>
+							</div>
+							<input type="hidden" name="cid" />
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
